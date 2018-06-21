699025f7a5036fdfc2ceeae200b88fc88be66631
Thomas Karpiniec <tk@1.21jiggawatts.net>
2018-06-21 03:27:38 -0400

    [A1] Generate a test C program too

--- a1-bind/build.py
+++ a1-bind/build.py
@@ -6,6 +6,7 @@ DESCRIPTION="TCP bind shellcode"
 import sys
 import os
 import socket
+import binascii
 
 if len(sys.argv) != 2:
     print "Usage: %s <port>" % sys.argv[0]
@@ -30,19 +31,39 @@ if r != 0:
     print "ld returned error"
     sys.exit(1)
 
-print "Extracting raw shellcode to %s.raw..." % TARGET
+print "Extracting text section to %s.raw..." % TARGET
 os.system("objcopy -j .text -O binary %s.elf %s.raw" % (TARGET, TARGET))
 
-print "\nC format:\n"
-os.system("hexdump -ve '1/1 \"\\\\x%%.2x\"' %s.raw" % TARGET)
-
-print "\n\nnasm format:\n"
-os.system("hexdump -ve '1/1 \"0x%%.2x,\"' %s.raw | sed -e 's/,$//'" % TARGET)
-
-print "\n"
+raw = ""
 with open("%s.raw" % TARGET, mode='rb') as f:
     raw = f.read()
-    if "\x00" in raw:
-        print "Null byte is present!"
-    else:
-        print "No nulls"
+
+hex_string = binascii.hexlify(raw)
+hex_pairs = [hex_string[i:i+2] for i in range(0, len(hex_string), 2)] 
+c_style = "".join(["\\x" + h for h in hex_pairs])
+nasm_style = ",".join(["0x" + h for h in hex_pairs])
+
+print "Embedding in test program shell_test.c"
+with open("shell_test.c", mode='w') as f:
+    f.write("#include <stdio.h>\n")
+    f.write("#include <string.h>\n")
+    f.write("unsigned char code[] = \"%s\";\n" % c_style)
+    f.write("int main(int argc, char *argv[]) {\n")
+    f.write("    printf(\"strlen(shellcode) = %d\\n\", strlen(code));\n")
+    f.write("    int (*ret)() = (int(*)())code;\n")
+    f.write("    ret();\n")
+    f.write("}\n")
+
+print "Compiling test program to shell_test..."
+r = os.system("gcc -fno-stack-protector -z execstack shell_test.c -o shell_test")
+if r != 0:
+    print "gcc returned error"
+    sys.exit(1)
+
+print "\nC format:\n%s\n" % c_style
+print "\nnasm format:\n%s\n" % nasm_style
+
+if "\x00" in raw:
+    print "WARNING: Null byte is present!"
+else:
+    print "No nulls detected."
